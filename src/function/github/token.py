53da264b5e43805c15efe278ca24830dac89b687
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-05-30 07:48:53 +0800

    style: Token 读取失败时返回 0

--- src/function/github/token.py
+++ src/function/github/token.py
@@ -7,9 +7,9 @@ def read_token():
         token = keyring.get_password("github-access-token.glm", "github-access-token")
         if token is None:
             print(f"你可能还没设置glm的Token, 请尝试使用以下命令设置Token:\n    glm config --token <YOUR-TOKEN>\n")
-            return "error"
+            return 0
         # else:
         return token
     except Exception as e:
         print(f"✕ 读取Token时出错:\n{e}")
-        return "error"
+        return 0
