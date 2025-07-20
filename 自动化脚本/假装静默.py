0915a396274d47be8235e97b9fb3aa1d25c00f10
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-07-20 18:35:56 +0800

    fix: Fix ModuleNotFoundError: No module named '修改版本号'

--- 自动化脚本/假装静默.py
+++ 自动化脚本/假装静默.py
@@ -2,9 +2,7 @@ import os
 import sys
 from 修改版本号 import 替换文件内容
 
-# 将 icon\Sundry.png 转为 icon\Sundry.ico
-
-源码路径 = os.path.normpath(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))), "Release", "src", "tools", "verify.py"))
+源码路径 = os.path.normpath(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))), "src", "tools", "verify.py"))
 
 替换文件内容(源码路径, "if input(f\"{Fore.BLUE}?{Fore.RESET} 临时清单目录下{Fore.YELLOW}已存在同名清单目录{Fore.RESET} {Fore.BLUE}{清单目录}{Fore.RESET}，我应该移除它吗? [Y/n]: \").lower()", "\"y\"")
 替换文件内容(源码路径, "input(\"按 ENTER 继续测试卸载，按 CTRL + C 结束...\")", "print(\"继续测试卸载...\")")
