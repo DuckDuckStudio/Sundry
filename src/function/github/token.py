577805861b7ff714061776d83845a5228a1fce33
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-10-29 00:02:20 +0800

    feat(func/github/token): 支持静默读取 Token，不报告错误

--- src/function/github/token.py
+++ src/function/github/token.py
@@ -2,15 +2,16 @@ import keyring
 from function.print.print import 消息头
 
 # GitHub 访问令牌
-def read_token():
+def read_token(silent: bool = False):
     # 凭据 github-access-token.glm
     try:
         token = keyring.get_password("github-access-token.glm", "github-access-token")
         if not token:
-            print("你可能还没设置glm的Token, 请尝试使用以下命令设置Token:\n    glm config --token <YOUR-TOKEN>\n")
+            if not silent:
+                print("你可能还没设置glm的Token, 请尝试使用以下命令设置Token:\n    glm config --token <YOUR-TOKEN>\n")
             return 0
-        # else:
         return token
     except Exception as e:
-        print(f"{消息头.错误} 读取Token时出错:\n{e}")
+        if not silent:
+            print(f"{消息头.错误} 读取Token时出错:\n{e}")
         return 0
