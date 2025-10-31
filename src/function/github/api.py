9dcf28d84bebfa56f0cfb71c4364573e3530d0ad
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-11-01 01:12:05 +0800

    fix(func/github/api): 在请求GitHubAPI函数中捕获 Exception 而不是不可能出现的 RequestException
    feat: github_token 接受 None 类型，视为请求时不带 Token

--- src/function/github/api.py
+++ src/function/github/api.py
@@ -3,7 +3,13 @@ import requests
 from typing import Any
 from exception.request import RequestException
 
-def 获取GitHub文件内容(github_token: str | int, 仓库: str, 文件路径: str) -> str | None:
+def 获取GitHub文件内容(github_token: str | int | None, 仓库: str, 文件路径: str) -> str | None:
+    """
+    尝试通过 GitHub API 获取文件 base64，解码后返回。
+
+    获取失败返回 `None`
+    """
+
     try:
         # 由于政府政策，在中国大陆不允许使用 raw.githubusercontent.com
         # 127.0.0.1 欢迎你 XD
@@ -16,9 +22,15 @@ def 获取GitHub文件内容(github_token: str | int, 仓库: str, 文件路径:
 
         return base64.b64decode(响应["content"]).decode("utf-8")
     except Exception:
-        return
+        return None
     
-def 请求GitHubAPI(apiUrl: str, github_token: str | int) -> Any | None:
+def 请求GitHubAPI(apiUrl: str, github_token: str | int | None = None) -> Any | None:
+    """
+    尝试向指定的 `apiUrl` 发送 `GET` 请求，返回 `响应.json()`
+
+    遇到 `Exception` 返回 `None`
+    """
+
     请求头: dict[str, str] = {
         "Authorization": f"token {github_token}",
         "Accept": "application/vnd.github.v3+json"
@@ -29,5 +41,5 @@ def 请求GitHubAPI(apiUrl: str, github_token: str | int) -> Any | None:
         响应 = requests.get(apiUrl, headers=请求头)
         响应.raise_for_status()
         return 响应.json()
-    except RequestException:
-        return
+    except Exception:
+        return None
