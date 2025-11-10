cd69ec2d02258badd81022bbaed807f6ba11aed8
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-11-11 05:38:08 +0800

    feat!(cleanup): 清理所有 Sundry 产生的文件时可以跳过缓存目录
    !: 现在不传递参数时视为 aec 而不是 all

--- src/tools/maintain/cleanup.py
+++ src/tools/maintain/cleanup.py
@@ -3,6 +3,7 @@ import shutil
 import tempfile
 from colorama import Fore, init
 from function.print.print import 消息头
+from function.maintain.config import 读取配置
 
 def main(哪个工具: str) -> int:
     """
@@ -14,15 +15,16 @@ def main(哪个工具: str) -> int:
 
     if not 哪个工具:
         print(f"{消息头.警告} 未指定清理哪个工具产生的文件，默认清理所有 Sundry 产生的文件")
-        哪个工具 = "all"
+        哪个工具 = "aec"
 
     哪个工具 = 哪个工具.lower()
-    
+
     工具别名 = {
         "logsAnalyse": ["日志分析", "logs-analyse", "log-analyse", "logs_analyse", "azure日志分析"],
         "remove": ["自动移除", "autoremove", "移除", "remove"],
         "verify": ["test", "验证", "测试"],
-        "all": ["所有", "all"]
+        "all": ["所有", "all"],
+        "aec": ["除了缓存", "all-except-cache"]
     }
     for 工具名, 别名 in 工具别名.items():
         if (哪个工具 in 别名) or (哪个工具 == 工具名.lower()):
@@ -31,24 +33,23 @@ def main(哪个工具: str) -> int:
     # 如不是已知别名，原样传递
 
     临时目录 = os.path.join(tempfile.gettempdir(), "Sundry")
-    
-    if 哪个工具 == "logsAnalyse":
-        待清理文件路径 = os.path.join(临时目录, "AzurePiplines", "Logs")
-    elif 哪个工具 == "remove":
-        待清理文件路径 = os.path.join(临时目录, "RemoveAndAutoRemove", "DownloadInstallers")
-    elif 哪个工具 == "verify":
-        待清理文件路径 = os.path.join(临时目录, "Verify")
-    elif 哪个工具 == "cache":
-        待清理文件路径 = os.path.join(临时目录, "Cache")
-    elif 哪个工具 == "all":
-        待清理文件路径 = 临时目录
-    else:
+    待清理文件路径: list[str] = []
+
+    if 哪个工具 in ["logsAnalyse", "all", "aec"]:
+        待清理文件路径.append(os.path.join(临时目录, "AzurePiplines", "Logs"))
+    if 哪个工具 in ["remove", "all", "aec"]:
+        待清理文件路径.append(os.path.join(临时目录, "RemoveAndAutoRemove", "DownloadInstallers"))
+    if 哪个工具 in ["verify", "all", "aec"]:
+        待清理文件路径.append(os.path.join(临时目录, "Verify"))
+    if 哪个工具 in ["cache", "all"]:
+        待清理文件路径.append(os.path.join(临时目录, "Cache"))
+    if not 待清理文件路径:
         print(f"{消息头.错误} 不知道该如何清理 {哪个工具} 产生的文件")
         return 1
-    
+
     return 清理文件(待清理文件路径, 哪个工具)
-    
-def 清理文件(待清理文件路径: str, 哪个工具: str) -> int:
+
+def 清理文件(待清理文件路径: list[str], 哪个工具: str) -> int:
     """
     删除指定的文件夹并打印日志。  
     参数 1: 指定待清理文件夹的路径  
@@ -56,15 +57,27 @@ def 清理文件(待清理文件路径: str, 哪个工具: str) -> int:
     """
 
     if 哪个工具 == "all":
-        哪个工具 = "所有 Sundry 产生的文件"
+        哪个工具 = "所有 Sundry"
+    elif 哪个工具 == "aec": # aka "All Except Cache"
+        哪个工具 = "除缓存外所有 Sundry"
 
-    try:
-        shutil.rmtree(待清理文件路径)
-        print(f"{消息头.成功} 成功清理 {哪个工具}")
-        return 0
-    except FileNotFoundError:
+    if 读取配置("debug"):
+        print(f"{消息头.调试} [clean] 清理 {哪个工具} 产生的文件 | 路径列表: {", ".join(待清理文件路径)}")
+
+    有清理 = False
+
+    for 路径 in 待清理文件路径:
+        try:
+            shutil.rmtree(路径)
+            有清理 = True
+        except FileNotFoundError:
+            continue
+        except Exception as e:
+            print(f"{消息头.错误} 清理 {哪个工具} 产生的文件时出现异常:\n{Fore.RED}{e}{Fore.RESET}")
+            return 1
+
+    if 有清理:
+        print(f"{消息头.成功} 成功清理 {哪个工具} 产生的文件")
+    else:
         print(f"{消息头.消息} 没有需要清理的文件")
-        return 0
-    except Exception as e:
-        print(f"{消息头.错误} 清理 {哪个工具} 时出现异常:\n{Fore.RED}{e}{Fore.RESET}")
-        return 1
+    return 0
