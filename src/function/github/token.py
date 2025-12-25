c834d196eb886dd9445e13fe212cd61afa337809
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-12-26 07:00:46 +0800

    feat(func/github/token): 支持从多个源读取 Token，具体从哪读由配置文件指定 (#224)

--- src/function/github/token.py
+++ src/function/github/token.py
@@ -1,22 +1,41 @@
-import keyring
+from colorama import Fore
 from typing import cast, Any
 from catfood.functions.print import 消息头
+from function.maintain.config import 读取配置
 from catfood.functions.github.api import 请求GitHubAPI
+from catfood.exceptions.operation import OperationFailed
 
-def read_token(silent: bool = False):
-    """尝试从钥匙环中读取 github-access-token.glm 密钥 (aka GitHub Token)"""
+def read_token(silent: bool = False) -> str | None:
+    """尝试从配置文件中指定的源读取 Token，读取失败返回 None"""
 
     try:
-        token = keyring.get_password("github-access-token.glm", "github-access-token")
-        if not token:
-            if not silent:
-                print("你可能还没设置glm的Token, 请尝试使用以下命令设置Token:\n    glm config --token <YOUR-TOKEN>\n")
-            return 0
-        return token
-    except Exception as e:
+        source: None | str | tuple[str, str] | bool = 读取配置("github.token")
+        if not isinstance(source, str):
+            raise OperationFailed("未能从配置文件中获取读取源")
+
+        if source == "env":
+            import os
+            if token := os.getenv("GITHUB_TOKEN"):
+                return token
+            else:
+                raise OperationFailed("没有读取到 Token，请确保您设置了 GITHUB_TOKEN 环境变量")
+        else:
+            import keyring
+            if source == "glm":
+                service_name, username = ("github-access-token.glm", "github-access-token")
+            elif source == "komac":
+                service_name, username = ("github-access-token.komac", "github-access-token")
+            else:
+                raise OperationFailed(f"未知的读取源 {source}")
+         
+            if token := keyring.get_password(service_name, username):
+                return token
+            else:
+                raise OperationFailed(f"没有读取到 Token，请确保您设置了 {source} 的 Token ({service_name}, {username})")
+    except OperationFailed as e:
         if not silent:
-            print(f"{消息头.错误} 读取Token时出错:\n{e}")
-        return 0
+            print(f"{消息头.错误} 读取 Token 失败: {Fore.RED}{e}{Fore.RESET}")
+        return None
 
 def 这是谁的Token(token: str | int | None) -> str | None:
     """
