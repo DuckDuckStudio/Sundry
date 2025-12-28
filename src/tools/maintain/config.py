ccfa897150bb50463f6a1502dbe47917682e31f5
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2025-12-28 17:21:14 +0800

    refactor(config): 提取转换配置值的步骤到 func/config 的函数中 (#235)
    在转换配置值时，该函数会尝试验证配置值，这
    fix(config): 修复在 初始化配置文件 和 更新配置值 时没有验证配置值的问题

--- src/tools/maintain/config.py
+++ src/tools/maintain/config.py
@@ -3,15 +3,20 @@ import json
 import jsonschema
 from typing import Any
 from colorama import Fore
+from function.maintain.config import (
+    配置信息,
+    转换配置值,
+    读取配置项, 读取配置,
+    获取当前配置版本, 获取配置schema
+)
 from catfood.functions.print import 消息头
 from catfood.functions.files import open_file
 from pygments import highlight # type: ignore
 from pygments.lexers import JsonLexer # type: ignore
 from pygments.formatters import TerminalFormatter
-from function.maintain.config import 读取配置, 验证配置, 读取配置项, 配置信息, 获取当前配置版本, 获取配置schema
+from catfood.exceptions.operation import OperationFailed
 
-# 获取用户输入
-def 获取用户输入(键路径: str) -> str | bool:
+def 获取用户输入(配置项: str) -> str | bool:
     提示消息映射: dict[str, str] = {
         # paths.*
         "paths.winget-pkgs": f"{消息头.问题} 您的本地 winget-{Fore.YELLOW}pkgs{Fore.RESET} 仓库在哪里: ",
@@ -39,60 +44,24 @@ def 获取用户输入(键路径: str) -> str | bool:
         "cache.validate.schema": f"{消息头.问题} 在 sundry validate 时是否缓存下载的清单架构 (schema)? (默认为{Fore.GREEN}是{Fore.RESET}): "
     }
 
-    if 键路径 in 提示消息映射:
-        提示消息 = 提示消息映射[键路径]
+    if 配置项 in 提示消息映射:
+        提示消息 = 提示消息映射[配置项]
     else:
-        提示消息 = f"{消息头.问题} 请输入 {键路径} 的值: "
+        提示消息 = f"{消息头.问题} 请输入 {配置项} 的值: "
+
+    # 给自己的兜底
+    if not 提示消息.endswith(" "):
+        if 读取配置("debug", 静默=True):
+            print(f"{消息头.内部警告} 配置项 {Fore.BLUE}{配置项}{Fore.RESET} 的提示消息未以空格结尾")
+        提示消息 += " "
 
     while True:
-        # 这个 while 用 return 而不是 break
+        配置值 = input(提示消息).strip()
+        try:
+            return 转换配置值(配置项, 配置值)
+        except OperationFailed as e:
+            print(f"{消息头.错误} 无法转换配置值: {Fore.RED}{e}{Fore.RESET}")
 
-        # 给自己的兜底
-        if not 提示消息.endswith(" "):
-            if 读取配置("debug", 静默=True):
-                print(f"{消息头.内部警告} 配置项 {Fore.BLUE}{键路径}{Fore.RESET} 的提示消息未以空格结尾")
-            提示消息 += " "
-
-        值 = input(提示消息).strip()
-
-        if 键路径.startswith("paths."):
-            路径 = os.path.abspath(os.path.normpath(值.replace("~", os.path.expanduser("~"))))
-            验证结果 = 验证配置(键路径, 路径)
-            if not 验证结果:
-                return 路径
-            else:
-                print(f"{消息头.错误} {验证结果}")
-        elif 键路径.startswith("repos."):
-            if 值.startswith("https://github.com/"):
-                值 = 值.replace("https://github.com/", "").replace(".git", "")
-            验证结果 = 验证配置(键路径, 值)
-            if not 验证结果:
-                return 值
-            else:
-                print(f"{消息头.错误} {验证结果}")
-        elif 键路径.startswith("cache."):
-            if 值.lower() in ["n", "no", "不要", "否", "false", "不"]:
-                return False
-            else:
-                return True
-        elif 键路径 in 配置信息.布尔值项:
-            if 值.lower() in ["y", "yes", "要", "是", "true"]:
-                return True
-            else:
-                return False
-        elif 键路径 == "i18n.lang":
-            if not 值:
-                值 = "zh-cn"
-            值 = 值.lower()
-            验证结果 = 验证配置(键路径, 值)
-            if not 验证结果:
-                return 值
-            else:
-                print(f"{消息头.错误} {验证结果}")
-        else:
-            return 值
-
-# 初始化配置文件
 def 初始化配置文件(配置文件: str) -> int:
     if not os.path.exists(配置文件) or (input(f"{消息头.警告} 已经存在了一份配置文件，要覆盖它吗? (默认为{Fore.GREEN}是{Fore.RESET}): ").lower() not in ["n", "no", "不要"]):
         默认配置: dict[str, Any] = 配置信息.默认配置
@@ -138,7 +107,6 @@ def 初始化配置文件(配置文件: str) -> int:
         print(f"\n{消息头.错误} 操作取消")
         return 1
 
-# 展示现有配置
 def 展示配置文件(配置文件: str) -> int:
     if os.path.exists(配置文件):
         try:
@@ -159,15 +127,16 @@ def 展示配置文件(配置文件: str) -> int:
         print(f"{消息头.提示} 运行 sundry config init 来初始化配置文件")
         return 1
 
-# 修改配置文件中的某一项
-def 修改配置项(条目: str, 值: str, 配置文件: str) -> int:
+def 修改配置项(配置项: str, 值: str, 配置文件: str) -> int:
     if os.path.exists(配置文件):
         try:
+            配置值 = 转换配置值(配置项, 值)
+
             with open(配置文件, "r", encoding="utf-8") as f:
                 配置数据 = json.load(f)
             
             # 解析嵌套键路径
-            键路径列表 = 条目.split(".")
+            键路径列表 = 配置项.split(".")
             当前字典 = 配置数据
             # NOTE 我对这里的操作不太理解，这似乎是同一个引用?
             # NOTE 反正现在这里这样写是可以的，改当前字典会影响配置数据。
@@ -175,35 +144,19 @@ def 修改配置项(条目: str, 值: str, 配置文件: str) -> int:
                 当前字典 = 当前字典[键]
             最后键 = 键路径列表[-1]
 
-            # 根据键路径类型转换值
-            if 条目 in 配置信息.布尔值项:
-                if 值.lower() in ["true", "yes", "y", "是", "要"]:
-                    配置值 = True
-                else:
-                    配置值 = False
-            elif 条目 == "i18n.lang":
-                验证结果 = 验证配置("i18n.lang", 值.lower())
-                if 验证结果:
-                    print(f"{消息头.错误} {验证结果}")
-                    return 1
-                else:
-                    配置值 = 值.lower()
-            else:
-                配置值 = 值
-
             当前字典[最后键] = 配置值
 
             with open(配置文件, "w", encoding="utf-8") as f:
                 json.dump(配置数据, f, indent=4, ensure_ascii=False)
             
-            print(f"{消息头.成功} 成功更新 {Fore.BLUE}{条目}{Fore.RESET} 为 {Fore.BLUE}{配置值}{Fore.RESET}")
+            print(f"{消息头.成功} 成功更新 {Fore.BLUE}{配置项}{Fore.RESET} 为 {Fore.BLUE}{配置值}{Fore.RESET}")
             return 0
         except json.decoder.JSONDecodeError as e:
             print(f"{消息头.错误} 读取配置文件失败，配置文件不是有效的 json 字段:\n{Fore.RED}{e}{Fore.RESET}")
             print(f"{消息头.提示} 请{Fore.YELLOW}考虑{Fore.RESET}运行 sundry config init 来覆盖现有的配置文件")
             return 1
         except Exception as e:
-            print(f"{消息头.错误} 更新条目失败:\n{Fore.RED}{e}{Fore.RESET}")
+            print(f"{消息头.错误} 更新配置失败: {Fore.RED}{e}{Fore.RESET}")
             return 1
     else:
         print(f"{消息头.错误} 配置文件不存在")
@@ -225,7 +178,7 @@ def 更新配置() -> int:
 
         schema = 获取配置schema(当前配置版本)
         if schema:
-            with open(配置信息.所在位置, "r", encoding="utf-8") as f:
+            with open(配置信息.所在位置, "r") as f:
                 jsonschema.validate(json.load(f), schema)
         else:
             print(f"{消息头.警告} 未能获取到当前配置版本的 schema，跳过验证")
@@ -278,7 +231,7 @@ def 更新配置() -> int:
         return 1
 
 def main(args: list[str]) -> int:
-    # 配置文件路径
+    # TODO: 在函数中直接读取这个信息
     配置文件 = 配置信息.所在位置
 
     try:
@@ -298,9 +251,7 @@ def main(args: list[str]) -> int:
             print(f"{消息头.信息} 尝试打开配置文件 config.json ...")
             return open_file(配置文件)
         elif len(args) == 2:
-            条目 = args[0]
-            值 = args[1]
-            return 修改配置项(条目, 值, 配置文件)
+            return 修改配置项(args[0], args[1], 配置文件)
         else:
             print(f"{消息头.错误} 无效的操作: {args[0]}")
             print(f"{消息头.提示} 运行 sundry --help 来获取命令帮助")
