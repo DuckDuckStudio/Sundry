50b6b5018d504c83879a26078325e4adf8693507
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2026-07-02 01:57:41 +0800

    feat: 让 PR 正文中的工具提示不要太碍眼 (#323)

--- src/function/constant/general.py
+++ src/function/constant/general.py
@@ -17,13 +17,14 @@ SUNDRY_VERSION: Final = "develop"
 Sundry 的版本
 """
 
-PR_TOOL_NOTE: Final = f"### This PR is automatically created by [Sundry](https://github.com/DuckDuckStudio/Sundry/) {f'{SUNDRY_VERSION} version' if SUNDRY_VERSION in ('develop', 'locale') else f'version {SUNDRY_VERSION}'} 🚀."
+# pylint: disable=line-too-long
+PR_TOOL_NOTE: Final = f"> This PR is automatically created by [Sundry](https://github.com/DuckDuckStudio/Sundry/) {f'{SUNDRY_VERSION} version' if SUNDRY_VERSION in ('develop', 'locale') else f'version {SUNDRY_VERSION}'} 🚀."
 """
 拉取请求正文中的 Sundry 工具说明。
 
 - 特殊版本
-### This PR is automatically created by [Sundry](https://github.com/DuckDuckStudio/Sundry/) locale/develop version 🚀.
+> This PR is automatically created by [Sundry](https://github.com/DuckDuckStudio/Sundry/) locale/develop version 🚀.
 
 - 一般版本
-### This PR is automatically created by [Sundry](https://github.com/DuckDuckStudio/Sundry/) version x.x.x 🚀.
+> This PR is automatically created by [Sundry](https://github.com/DuckDuckStudio/Sundry/) version x.x.x 🚀.
 """
