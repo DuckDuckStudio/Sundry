423d1abb28393eb20e90e1b79366a209b6df3e9e
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2026-09-05 07:14:38 +0800

    refactor: 移除不可能匹配的别名 Version (#391)
    - Fixes https://github.com/DuckDuckStudio/Sundry/issues/389

--- src/sundry.py
+++ src/sundry.py
@@ -74,7 +74,7 @@ def main() -> int:
         arg = args[0] if args else ""
         return cleanup.main(arg) # 仅接受单个 str 参数
     # 其他
-    elif tool in ("ver", "版本", "version", "Version", "--version", "--ver", "-v"):
+    elif tool in ("ver", "版本", "version", "--version", "--ver", "-v"):
         print("Sundry by 鸭鸭「カモ」")
         print(f"版本: {SUNDRY_VERSION}")
         print(f"安装在: {SUNDRY_LOCATION}")
